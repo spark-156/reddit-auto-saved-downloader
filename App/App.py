81872613de1526ed4a83d2e48615b36a968eed77
spark-156 <lucabergman@hotmail.nl>
2021-04-16 12:54:31 +0200

    commit

--- App/App.py
+++ App/App.py
@@ -1,50 +1,62 @@
 import praw
 import os
 import json
+
 class RedditUser:
-    def __init__(self, reddit_username, reddit_password, client_id, client_secret):
-			self.saved_posts = {}
+	def __init__(self, account):
+		self.saved_posts = {}
+		self.reddit_username = account["username"]
+		self.reddit = praw.Reddit(
+				client_id=account["client_id"],
+				client_secret=account["client_secret"],
+				user_agent="Auto reddit saved downloader",
+				password=account["password"],
+				username=account["username"]
+		)
+		# init file if not exist
+		if not os.path.isfile(f"saved_posts_{self.reddit_username}.json"):
+			with open(f"saved_posts_{self.reddit_username}.json", "w") as saved_posts_file:
+				json.dump({}, saved_posts_file)
 
-			# init praw instance
-			self.reddit = praw.Reddit(
-					client_id=client_id,
-					client_secret=client_secret,
-					user_agent="Auto reddit saved downloader",
-					reddit_password=reddit_password,
-					reddit_username=reddit_username
-			)
+	# Get all saved posts
+	# @param limit - get latest {limit} amount of saved posts. Set to None if you want to get all saved posts
+	# @returns nothing
+	def get_saved_posts(self, limit=None):
+		self.log("getting saved posts from reddit")
+		with open(f"saved_posts_{self.reddit_username}.json", "r") as saved_posts_file:
+			self.saved_posts = json.load(saved_posts_file)
 
-			# init file if not exist
-			if not os.path.isfile(f"saved_posts_{reddit_username}.json"):
-					with open(f"saved_posts_{reddit_username}.json", "w") as saved_posts_file:
-							json.dump({}, saved_posts_file)
+		i = 0
+		saved = self.reddit.user.me().saved(limit=limit)
+		for item in saved:
+			submission = praw.models.Submission(self.reddit, item.id)
 
-		# Get all saved posts
-		# @param limit - get latest {limit} amount of saved posts. Set to None if you want to get all saved posts
-		# @returns nothing
-		def get_saved_posts(limit=None):
-			with open(f"saved_posts_{self.reddit_username}.json", "r") as saved_posts_file:
-				self.saved_posts = json.load(saved_posts_file)
-
-			i = 0
-			for item in self.reddit.user.me().saved(limit=limit):
-				submission = praw.models.Submission(self.reddit, item.id)
-
-				# Check if post is already saved, if it is skip it
-				if submission.id in saved_posts: # if guard
-					continue
-				
-				self.saved_posts[submission.id] = submission
-				i += 1
+			# Check if post is already saved, if it is skip it
+			if submission.id in self.saved_posts: # if guard
+				continue
 			
-			with open (f"saved_posts_{self.reddit_username}.json", "w"):
-				json.dump(self.saved_posts, saved_posts_file)
+			self.saved_posts[submission.id] = submission
+			i += 1
+		self.log(f"saving {i} saved posts to local cache")
+		with open (f"saved_posts_{self.reddit_username}.json", "w") as saved_posts_file:
+			json.dump(self.saved_posts, saved_posts_file)
 
-		# Get all saved posts present in json file
-		# @return dictionary with all post id's as keys and submission object as values.
-		def get_cached_saved_posts():
-			with open(f"saved_posts_{self.reddit_username}", "r") as saved_posts_file:
-				self.saved_posts = json.load(saved_posts_file)
+	# Get all saved posts present in json file
+	# @return dictionary with all post id's as keys and submission object as values.
+	def get_cached_saved_posts(self):
+		self.log("getting cached saved posts")
+		with open(f"saved_posts_{self.reddit_username}", "r") as saved_posts_file:
+			self.saved_posts = json.load(saved_posts_file)
+		return self.saved_posts
 
-			return self.saved_posts
+	def log(self, message):
+		print(f"[Log] {message}")
+	
+
+with open("reddit_accounts.json", "r") as reddit_accounts_file:
+	reddit_accounts = json.load(reddit_accounts_file)
+
+for account in reddit_accounts:
+	user = RedditUser(account)
+	user.get_saved_posts(5)
 
